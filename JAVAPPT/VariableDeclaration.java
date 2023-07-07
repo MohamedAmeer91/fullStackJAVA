45f0f34535803b3d37658f1bada467edc49dc280
Mohamed Ameer <132927275+MohamedAmeer91@users.noreply.github.com>
2023-07-07 16:16:03 +0530

    Create VariableDeclaration.java

---
+++ JAVAPPT/VariableDeclaration.java
@@ -0,0 +1,27 @@
+package in.Ameer.MockTest;
+
+public class VariableDeclaration {
+	
+	public static void main(String[] args) {
+		
+		//  program to declare and initialize variables of different data types (int, double, String) 
+		
+		int a,b;
+		double salary, bonus;
+		String First_name, Last_name;
+		
+		a=5;
+		b=10;
+		
+		salary = 123456.78d;
+		bonus = 15278.65d;
+		
+		First_name = "Jeff";
+		Last_name = "Bezoz";
+		
+		System.out.println("Integer values::"+a+" ;"+b);
+		System.out.println("Double values::"+salary+" ; "+bonus);
+		System.out.println("String values::"+First_name+ " "+Last_name);
+	}
+
+}
